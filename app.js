8a6088b58f05a9f8a120c097e8388a7df6a8a31b
Simon Legner <Simon.Legner@gmail.com>
2018-04-19 22:10:25 +0200

    L.BldrwnschLayer class

--- app.js
+++ app.js
@@ -67,46 +67,51 @@ L.control
   })
   .addTo(map);
 
-var markers = new PruneClusterForLeaflet().addTo(map);
-markers.PrepareLeafletMarker = function(marker, row) {
-  var camera =
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Photo-request.svg/32px-Photo-request.svg.png';
-  marker.setIcon(
-    L.icon({
-      iconUrl: camera
-    })
-  );
-  var link =
-    '<a href="https://de.wikipedia.org/wiki/' +
-    row.title +
-    '" target="_blank">' +
-    row.title +
-    '</a>';
-  if (row.description && L.Browser.mobile) {
-    marker.bindPopup(row.description + '<br>' + link);
-  } else if (row.description) {
-    marker.bindTooltip(row.description + '<br>' + row.title);
-    marker.bindPopup(row.description + '<br>' + link);
-  } else if (L.Browser.mobile) {
-    marker.bindPopup(link);
-  } else {
-    marker.bindTooltip(row.title);
-    marker.bindPopup(link);
+L.BldrwnschLayer = PruneClusterForLeaflet.extend({
+  PrepareLeafletMarker: function(marker, row) {
+    var camera =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Photo-request.svg/32px-Photo-request.svg.png';
+    marker.setIcon(
+      L.icon({
+        iconUrl: camera
+      })
+    );
+    var link =
+      '<a href="https://de.wikipedia.org/wiki/' +
+      row.title +
+      '" target="_blank">' +
+      row.title +
+      '</a>';
+    if (row.description && L.Browser.mobile) {
+      marker.bindPopup(row.description + '<br>' + link);
+    } else if (row.description) {
+      marker.bindTooltip(row.description + '<br>' + row.title);
+      marker.bindPopup(row.description + '<br>' + link);
+    } else if (L.Browser.mobile) {
+      marker.bindPopup(link);
+    } else {
+      marker.bindTooltip(row.title);
+      marker.bindPopup(link);
+    }
+  },
+  fetchData: function() {
+    var cluster = this;
+    fetch('https://tools.wmflabs.org/bldrwnsch/Bilderwuensche.json')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.forEach(function(row) {
+          var data = {
+            description: (row.description || '').replace(/_/g, ' '),
+            title: (row.title || '').replace(/_/g, ' ')
+          };
+          var marker = new PruneCluster.Marker(row.lat, row.lon, data);
+          cluster.RegisterMarker(marker);
+        });
+        cluster.ProcessView();
+      });
   }
-};
+});
 
-fetch('https://tools.wmflabs.org/bldrwnsch/Bilderwuensche.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    json.forEach(function(row) {
-      var data = {
-        description: (row.description || '').replace(/_/g, ' '),
-        title: (row.title || '').replace(/_/g, ' ')
-      };
-      var marker = new PruneCluster.Marker(row.lat, row.lon, data);
-      markers.RegisterMarker(marker);
-    });
-    markers.ProcessView();
-  });
+new L.BldrwnschLayer().addTo(map).fetchData();
