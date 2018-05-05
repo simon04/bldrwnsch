7d891b9f85fadff83bb1ba97c1fd3429e4f798bb
Simon Legner <Simon.Legner@gmail.com>
2018-05-05 23:38:48 +0200

    Wrap in BldrwnschLayer

--- app.js
+++ app.js
@@ -35,79 +35,78 @@ L.control
   })
   .addTo(map);
 
-var markers = L.geoJson(null, {
-  pointToLayer: pointToLayer
-}).addTo(map);
-
-var worker = new Worker('bldrwnschCluster.js');
-
-worker.onmessage = function(e) {
-  if (e.data.ready) {
-    map.on('moveend', update);
-    update();
-  } else if (e.data.expansionZoom) {
-    map.flyTo(e.data.center, e.data.expansionZoom);
-  } else {
-    markers.clearLayers();
-    markers.addData(e.data);
-  }
-};
-
-function update() {
-  var bounds = map.getBounds();
-  worker.postMessage({
-    bbox: [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()],
-    zoom: map.getZoom()
-  });
-}
-
-worker.onerror = function(e) {
-  console.warn(e);
-};
-
-function pointToLayer(feature, latlng) {
-  if (feature.properties.cluster) {
-    return createClusterIcon(feature, latlng);
-  } else {
-    return createIcon(feature, latlng);
-  }
-}
-
-function createClusterIcon(feature, latlng) {
-  var count = feature.properties.point_count;
-  var size = count < 100 ? 'small' : count < 1000 ? 'medium' : 'large';
-  var icon = L.divIcon({
-    html: '<div><span>' + feature.properties.point_count_abbreviated + '</span></div>',
-    className: 'marker-cluster marker-cluster-' + size,
-    iconSize: L.point(40, 40)
-  });
-  return L.marker(latlng, {icon: icon});
-}
-
-function createIcon(feature, latlng) {
-  var camera =
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Photo-request.svg/32px-Photo-request.svg.png';
-  var icon = L.icon({iconUrl: camera});
-  var marker = L.marker(latlng, {icon: icon});
-  var data = feature.properties;
-  var description = data.description ? data.description.replace(/_/g, ' ') + '<br>' : '';
-  var title = data.title ? data.title.replace(/_/g, ' ') : '';
-  var link =
-    '<a href="https://de.wikipedia.org/wiki/' + title + '" target="_blank">' + title + '</a>';
-  if (L.Browser.mobile) {
-    marker.bindPopup(description + link);
-  } else {
-    marker.bindTooltip(description + title);
-    marker.bindPopup(description + link);
-  }
-  return marker;
-}
-
-markers.on('click', function(e) {
-  if (e.layer.feature.properties.cluster_id) {
-    worker.postMessage({
-      getClusterExpansionZoom: e.layer.feature.properties.cluster_id,
-      center: e.latlng
+var BldrwnschLayer = L.GeoJSON.extend({
+  initialize: function() {
+    L.GeoJSON.prototype.initialize.call(this, null, {
+      pointToLayer: function(feature, latlng) {
+        if (feature.properties.cluster) {
+          return this.createClusterIcon(feature, latlng);
+        } else {
+          return this.createIcon(feature, latlng);
+        }
+      }.bind(this)
     });
+  },
+  onAdd: function(map) {
+    var worker = new Worker('bldrwnschCluster.js');
+    worker.onmessage = function(e) {
+      if (e.data.ready) {
+        map.on('moveend', update);
+        update();
+      } else if (e.data.expansionZoom) {
+        map.flyTo(e.data.center, e.data.expansionZoom);
+      } else {
+        this.clearLayers();
+        this.addData(e.data);
+      }
+    }.bind(this);
+    worker.onerror = function(e) {
+      console.warn(e);
+    };
+    function update() {
+      var bounds = map.getBounds();
+      worker.postMessage({
+        bbox: [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()],
+        zoom: map.getZoom()
+      });
+    }
+    this.on('click', function(e) {
+      if (e.layer.feature.properties.cluster_id) {
+        worker.postMessage({
+          getClusterExpansionZoom: e.layer.feature.properties.cluster_id,
+          center: e.latlng
+        });
+      }
+    });
+  },
+  createClusterIcon: function(feature, latlng) {
+    var count = feature.properties.point_count;
+    var size = count < 100 ? 'small' : count < 1000 ? 'medium' : 'large';
+    var icon = L.divIcon({
+      html: '<div><span>' + feature.properties.point_count_abbreviated + '</span></div>',
+      className: 'marker-cluster marker-cluster-' + size,
+      iconSize: L.point(40, 40)
+    });
+    return L.marker(latlng, {icon: icon});
+  },
+  createIcon: function(feature, latlng) {
+    var camera =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Photo-request.svg/32px-Photo-request.svg.png';
+    var icon = L.icon({iconUrl: camera});
+    var marker = L.marker(latlng, {icon: icon});
+    var data = feature.properties;
+    var description = data.description ? data.description.replace(/_/g, ' ') + '<br>' : '';
+    var title = data.title ? data.title.replace(/_/g, ' ') : '';
+    var link =
+      '<a href="https://de.wikipedia.org/wiki/' + title + '" target="_blank">' + title + '</a>';
+    if (L.Browser.mobile) {
+      marker.bindPopup(description + link);
+    } else {
+      marker.bindTooltip(description + title);
+      marker.bindPopup(description + link);
+    }
+    return marker;
   }
 });
+
+new BldrwnschLayer().addTo(map);
