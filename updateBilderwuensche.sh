87420cae67e2b8f9251064c3ed1653d0cb487cb1
Simon Legner <Simon.Legner@gmail.com>
2018-09-18 20:30:42 +0200

    updateBilderwuensche: +cd

--- updateBilderwuensche.sh
+++ updateBilderwuensche.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Updates Bilderwuensche.json.gz by fetching Bilderwuensche.csv, converting to JSON and gzipping
+cd "$(dirname $0)"
 curl --silent https://tools.wmflabs.org/request/bwAPI/export/Bilderwuensche.csv \
   | jq --raw-input --slurp --from-file csv2json.jq --compact-output \
   | gzip > Bilderwuensche.json.gz
