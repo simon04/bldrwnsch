fdc859ed44ce45a941bc8411506b1ab6913195c2
Simon Legner <Simon.Legner@gmail.com>
2019-04-23 22:10:59 +0200

    Apply eslint and eslint-plugin-compat

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:compat/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': 0
+  }
+};
--- updateBilderwuensche.js
+++ updateBilderwuensche.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-env node */
 
 const fs = require('fs');
 const readline = require('readline');
