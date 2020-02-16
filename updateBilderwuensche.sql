2d8c4cbcadf01b64d3d92771ab6cdaf8d414802d
Simon Legner <Simon.Legner@gmail.com>
2020-02-16 17:26:37 +0100

    Improve performance in updateBilderwuensche.sql

--- updateBilderwuensche.sql
+++ updateBilderwuensche.sql
@@ -1,3 +1,4 @@
 SELECT page.page_title, pagelinks.pl_title
 FROM pagelinks JOIN page ON pagelinks.pl_from = page.page_id
-WHERE pagelinks.pl_title LIKE 'Bilderwunsch/code%';
+WHERE page.page_id IN (SELECT cl_from FROM categorylinks WHERE cl_to = 'Wikipedia:Bilderwunsch_an_bestimmtem_Ort')
+AND pagelinks.pl_title LIKE 'Bilderwunsch/code%';
