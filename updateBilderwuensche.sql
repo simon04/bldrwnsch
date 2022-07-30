4f19e20962aee3058d28245540d0e5cf16c2961b
Simon Legner <Simon.Legner@gmail.com>
2022-07-30 08:46:57 +0200

    Load from all Kategorie:Wikipedia:Bilderwunsch*
    https://de.wikipedia.org/w/index.php?title=Wikipedia_Diskussion:Bilderw%C3%BCnsche&diff=224921151&oldid=222848292&diffmode=source

--- updateBilderwuensche.sql
+++ updateBilderwuensche.sql
@@ -1,4 +1,4 @@
 SELECT page.page_title, pagelinks.pl_title
 FROM pagelinks JOIN page ON pagelinks.pl_from = page.page_id
-WHERE page.page_id IN (SELECT cl_from FROM categorylinks WHERE cl_to = 'Wikipedia:Bilderwunsch_an_bestimmtem_Ort')
+WHERE page.page_id IN (SELECT cl_from FROM categorylinks WHERE cl_to LIKE 'Wikipedia:Bilderwunsch%')
 AND pagelinks.pl_title LIKE 'Bilderwunsch/code%';
