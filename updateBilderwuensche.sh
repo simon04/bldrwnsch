86aadaa5edc99ebb1e6620b83ecc8fe4ab0e9dc9
Simon Legner <Simon.Legner@gmail.com>
2023-12-03 12:16:22 +0100

    Logging

--- updateBilderwuensche.sh
+++ updateBilderwuensche.sh
@@ -14,12 +14,16 @@ curl -sS --max-time 3 "${SENTRY_CRONS}?status=in_progress"
 # toolforge envvars list
 MYSQL_USER=$TOOL_REPLICA_USER MYSQL_PASSWORD=$TOOL_REPLICA_PASSWORD MYSQL_HOST=dewiki.analytics.db.svc.wikimedia.cloud MYSQL_DATABASE=dewiki_p ./updateBilderwuensche
 
-source .venv/bin/activate
+echo $(date -Is) Building Bilderwuensche.tiles
 $TIPPECANOE --no-progress-indicator --output-to-directory=Bilderwuensche.tiles --force --layer=Bilderwuensche --maximum-zoom=10 --no-tile-compression Bilderwuensche.geojson
+echo $(date -Is) Building Bilderwuensche.pmtiles
 $TIPPECANOE --no-progress-indicator --force --layer=Bilderwuensche --maximum-zoom=10 --no-tile-compression --output=Bilderwuensche.pmtiles Bilderwuensche.geojson
 gzip --force --keep Bilderwuensche.pmtiles
+
+echo $(date -Is) Copying Bilderwuensche to $DESTDIR
 cp --force --recursive --preserve=all Bilderwuensche.tsv Bilderwuensche.json Bilderwuensche.json.gz Bilderwuensche.geojson Bilderwuensche.geojson.gz Bilderwuensche.gpx Bilderwuensche.gpx.gz Bilderwuensche.kml Bilderwuensche.kmz Bilderwuensche.pmtiles Bilderwuensche.pmtiles.gz $DESTDIR
 rm -rf $DESTDIR/Bilderwuensche.tiles/
 mv Bilderwuensche.tiles/ $DESTDIR/Bilderwuensche.tiles/
 
 curl -sS --max-time 3 "${SENTRY_CRONS}?status=ok"
+curl -sS --max-time 3 "https://hc-ping.com/ab41e9ba-b103-40ae-85ed-842ceae93a4f"
