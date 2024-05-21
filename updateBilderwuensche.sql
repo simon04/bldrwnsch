fb08b36d57b3eae27af435b252b5cb095b52dae1
Simon Legner <Simon.Legner@gmail.com>
2024-05-21 18:13:57 +0200

    pagelinks.pl_title → linktarget.lt_title
    https://phabricator.wikimedia.org/T365497
    
    https://meta.wikimedia.org/wiki/Tech/News/2024/20

--- updateBilderwuensche.sql
+++ updateBilderwuensche.sql
@@ -1,6 +1,6 @@
 SELECT page.page_title, linktarget.lt_title, geo_tags.gt_lat, geo_tags.gt_lon
 FROM page
-JOIN pagelinks ON pagelinks.pl_target_id = page.page_id
+JOIN pagelinks ON pagelinks.pl_from = page.page_id
 JOIN linktarget ON linktarget.lt_id = pagelinks.pl_target_id
 LEFT JOIN geo_tags ON geo_tags.gt_primary=1 AND geo_tags.gt_globe='earth' AND geo_tags.gt_page_id=page.page_id
 WHERE page.page_id IN (SELECT cl_from FROM categorylinks WHERE cl_to LIKE 'Wikipedia:Bilderwunsch%')
