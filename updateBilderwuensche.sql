e4ff7a5344192aa8637a54c52367cb1ac7bd603d
Simon Legner <Simon.Legner@gmail.com>
2026-03-16 16:28:28 +0100

    categorylinks.cl_to → categorylinks.cl_target_id
    https://phabricator.wikimedia.org/T420145

--- updateBilderwuensche.sql
+++ updateBilderwuensche.sql
@@ -1,5 +1,5 @@
 WITH pages_with_bilderwunsch AS (
-  SELECT categorylinks.cl_from FROM categorylinks 
+  SELECT categorylinks.cl_from AS page_id FROM categorylinks 
   JOIN linktarget ON categorylinks.cl_target_id = linktarget.lt_id
   WHERE linktarget.lt_namespace = 14 AND linktarget.lt_title LIKE 'Wikipedia:Bilderwunsch%'
 )
@@ -8,5 +8,5 @@ FROM page
 JOIN pagelinks ON pagelinks.pl_from = page.page_id
 JOIN linktarget ON linktarget.lt_id = pagelinks.pl_target_id
 LEFT JOIN geo_tags ON geo_tags.gt_primary=1 AND geo_tags.gt_globe='earth' AND geo_tags.gt_page_id=page.page_id
-WHERE page.page_id IN (SELECT tl_from FROM pages_with_bilderwunsch)
+WHERE page.page_id IN (SELECT page_id FROM pages_with_bilderwunsch)
 AND linktarget.lt_title LIKE 'Bilderwunsch/code%';
